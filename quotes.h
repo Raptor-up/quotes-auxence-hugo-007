cf0160429e9f837be3560efa1c6db09d537eea46
auxencecolson <auxence.colson@efrei.net>
2025-03-25 12:12:38 +0100

    2nd

---
+++ quotes.h
@@ -0,0 +1,21 @@
+//
+// Created by auxen on 25/03/2025.
+//
+
+#ifndef QUOTES_H
+#define QUOTES_H
+// quotes.h
+ #ifndef QUOTES_H
+ #define QUOTES_H
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+
+ #define MAX_QUOTES 10
+
+ void print_quote(char ** quotes, int index);
+void print_random_quote(char ** quotes);
+
+#endif
+#endif //QUOTES_H
